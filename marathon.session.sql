7658c66e25e783e1034dc496ed5a1bd8e6ab002d
crimsonsm12 <79145606+crimsonsm12@users.noreply.github.com>
2025-04-06 22:04:57 -0400

    Add files via upload

--- marathon.session.sql
+++ marathon.session.sql
@@ -24,23 +24,6 @@ select * from mission_launches LIMIT 20;
 
 --Count of missions launched by organizations
 
-SELECT
-organization,
-DATE,
-COUNT(rockets_mission) as max_missions
-FROM
-mission_launches
-GROUP BY organization, DATE
-ORDER BY max_missions DESC
-
-
-Select
-    extract(MONTH from date) as month_d
-from
-mission_launches
-
---COUNT missions flown by an organization by year
-
 select
 organization,
 COUNT(rockets_mission) as max_missions,
@@ -48,25 +31,182 @@ extract(YEAR from date) as year_d
 FROM
 mission_launches
 GROUP BY extract(YEAR from date), organization
-ORDER BY year_d DESC, max_missions DESC;
+ORDER BY year_d DESC;
 
---Max mission launches in a year by organization:
+
+--Max mission launches by year:
 select
-organization,
 year_d,
-MAX(rockets_missions) as max_mission
+MAX(rockets_missions) as maX
 FROM
 (select
-organization,
 COUNT(rockets_mission) as rockets_missions,
 extract(YEAR from date) as year_d
 FROM
 mission_launches
-GROUP bY extract(Year from date), organization)
+GROUP bY extract(Year from date))
 mission_launches
-GROUP BY organization, year_d
-ORDER by year_d DESC
+GROUP BY year_d
+ORDER BY max DESC;
+
+/*WITH cte_name AS (
+    SELECT 
+        column1, column2, 
+        window_function() OVER (PARTITION BY column_name ORDER BY column_name) AS alias
+    FROM table_name
+)
+
+SELECT * FROM cte_name;
+*/
+
+
+
+--max mission launches of organizations by year 
+
+WITH yearly_missions AS (
+    SELECT  
+        EXTRACT(YEAR FROM date) AS year_d, 
+        organization,
+        COUNT(rockets_mission) AS mission_count
+    FROM mission_launches
+    GROUP BY year_d, organization
+),
+ranked_missions AS (
+    SELECT 
+        year_d, 
+        organization, 
+        mission_count,
+        ROW_NUMBER() OVER (PARTITION BY year_d ORDER BY mission_count DESC) AS rnk
+    FROM yearly_missions
+)
+SELECT year_d, organization, mission_count 
+FROM ranked_missions
+WHERE rnk = 1
+ORDER BY year_d DESC;
+
 
 --How has the cost of space missions varied over time?
 
+WITH am AS(
+    SELECT 
+        organization,
+        MAX(price::DECIMAL(10,2)) AS max_price,
+        EXTRACT(YEAR FROM date) AS year_d   
+    FROM mission_launches
+    WHERE price IS NOT NULL
+    GROUP BY organization, year_d
+),
+max_prices AS(
+    select
+    organization,
+    max_price,
+    year_d,
+    ROW_NUMBER () OVER(PARTITION BY year_d ORDER BY max_price DESC) as rn
+FROM am
+)
 select
+    organization, year_d, max_price
+FROM
+max_prices
+where rn = 1
+order by year_d DESC
+
+--Which organization had the most successful missions
+
+WITH status_count AS(
+    select
+    organization,
+    EXTRACT(YEAR FROM date) AS year_d,
+    COUNT(Mission_Status) as num_status
+    FROM
+    mission_launches
+    where Mission_Status LIKE '%Success%'
+    GROUP BY organization, year_d
+),
+Ranking_status AS(
+    select
+    organization,
+    year_d,
+    num_status,
+    ROW_NUMBER() OVER(PARTITION BY year_d ORDER BY num_status DESC) as rp
+    FROM status_count
+) 
+select
+organization,
+year_d,
+num_status
+FROM Ranking_status
+where rp =1
+order by year_d DESC
+
+---without year:
+
+WITH status_count AS(
+    select
+    organization,
+    COUNT(Mission_Status) as num_status
+    FROM
+    mission_launches
+    where Mission_Status LIKE '%Success%'
+    GROUP BY organization
+),
+Ranking_status AS(
+    select
+    organization,
+    num_status,
+    ROW_NUMBER() OVER(PARTITION BY organization ORDER BY num_status DESC) as rp
+    FROM status_count
+) 
+select
+organization,
+num_status
+FROM Ranking_status
+where rp =1
+
+
+----Which organization had the most failure missions
+
+WITH status_count AS(
+    select
+    organization,
+    COUNT(Mission_Status) as num_status
+    FROM
+    mission_launches
+    where Mission_Status LIKE '%Failure%'
+    GROUP BY organization
+),
+Ranking_status AS(
+    select
+    organization,
+    num_status,
+    ROW_NUMBER() OVER(PARTITION BY organization ORDER BY num_status DESC) as rp
+    FROM status_count
+) 
+select
+organization,
+num_status
+FROM Ranking_status
+where rp =1
+
+--Most launches by country
+WITH country_launches AS(
+SELECT substring( location FROM '[^ ]+$') AS country,
+COUNT(mission_launches) as count_launches
+from mission_launches
+where substring( location FROM '[^ ]+$') not LIKE '%Facility%' AND
+substring( location FROM '[^ ]+$') not LIKE '%Site%' AND
+substring( location FROM '[^ ]+$') not LIKE '%Sea%' AND
+substring( location FROM '[^ ]+$') not LIKE '%Ocean'
+GROUP BY country),
+ranked_launch AS(
+select
+country,
+count_launches,
+ROW_NUMBER() OVER(PARTITION BY country ORDER BY count_launches DESC) as rank_count
+FROM country_launches)
+select
+country,
+count_launches
+from
+ranked_launch
+where rank_count = 1
